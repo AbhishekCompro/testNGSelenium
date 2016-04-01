c8ffd8d733a0e36272b86686f6bdb58a43f39bb4
Abhishek <abhishek.kumar@comprotechnologies.com>
2016-04-01 13:23:29 +0530

    updating test for compro reader

--- src/test/java/com/testng/selenium/TestExtractElementsCfg.java
+++ src/test/java/com/testng/selenium/TestExtractElementsCfg.java
@@ -41,20 +41,43 @@ public class TestExtractElementsCfg {
 		}
 	}
 	
+	@Test
+	public void searchTestExtractElements() throws InterruptedException {
+		
+		driver.navigate().to("http://reader.comprodls.com/");
+		logger.info("Extracting elements for Page: http://www.github.com "+driver.getTitle());
+		
+		extractElements(driver);
+		
+		Thread.sleep(15000);
+		readerLogin(driver);
+		Thread.sleep(15000);
+		
+		logger.info("Extracting elements for Reader Page after Login");
+		extractElements(driver);
+		
+		logger.info("Done.");
+	}
+
+	@AfterSuite
+	public void quitDriver() throws Exception {
+		driver.quit();
+	}
+
+	/*
+	 * http://stackoverflow.com/questions/4176560/webdriver-get-elements-xpath 
+	 */
 	public String getElementXPath(WebDriver driver, WebElement element) {
 	    return (String)((JavascriptExecutor)driver).executeScript("gPt=function(c){if(c.id!==''){return'id(\"'+c.id+'\")'}if(c===document.body){return c.tagName}var a=0;var e=c.parentNode.childNodes;for(var b=0;b<e.length;b++){var d=e[b];if(d===c){return gPt(c.parentNode)+'/'+c.tagName+'['+(a+1)+']'}if(d.nodeType===1&&d.tagName===c.tagName){a++}}};return gPt(arguments[0]).toLowerCase();", element);
 	}
-
-	@Test
-	public void searchTestExtractElements() {
+	
+	public void extractElements(WebDriver driver){
 		
-		driver.navigate().to("http://www.github.com");
-		logger.info("Extracting elements for Page: http://www.github.com "+driver.getTitle());
 		logger.info("########## ########## ##########");
 		
-		List<WebElement> input_elements = driver.findElements(By.tagName("input"));
-		logger.info("Total number of <input> elements: "+input_elements.size());
-		for(WebElement el : input_elements) {
+		List<WebElement> button_elements = driver.findElements(By.tagName("button"));
+		logger.info("Total number of <button> elements: "+button_elements.size());
+		for(WebElement el : button_elements) {
             logger.info(el.getText());
             String path = getElementXPath(driver,el);
             path = "/html/" + path;
@@ -74,9 +97,9 @@ public class TestExtractElementsCfg {
         }
 		logger.info("########## ########## ##########");
 		
-		List<WebElement> paragraph_elements = driver.findElements(By.tagName("p"));
-		logger.info("Total number of <p> elements: "+paragraph_elements.size());
-		for(WebElement el : paragraph_elements) {
+		List<WebElement> list_elements = driver.findElements(By.tagName("li"));
+		logger.info("Total number of <li> elements: "+list_elements.size());
+		for(WebElement el : list_elements) {
             logger.info(el.getText());
             String path = getElementXPath(driver,el);
             path = "/html/" + path;
@@ -84,12 +107,29 @@ public class TestExtractElementsCfg {
             logger.info("");
         }
 		logger.info("########## ########## ##########");
-
-		logger.info("Done.");
+		
+		List<WebElement> input_elements = driver.findElements(By.tagName("input"));
+		logger.info("Total number of <input> elements: "+input_elements.size());
+		for(WebElement el : input_elements) {
+            logger.info(el.getText());
+            String path = getElementXPath(driver,el);
+            path = "/html/" + path;
+            logger.info("xpath: "+ path);
+            logger.info("");
+        }
+		logger.info("########## ########## ##########");
+	};
+	
+	public void readerLogin(WebDriver driver) throws InterruptedException{
+		
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@id='login-form']/fieldset/section[1]/label[2]/input")).sendKeys("test6");
+		driver.findElement(By.xpath("//*[@id='login-form']/fieldset/section[2]/label[2]/input")).sendKeys("Student1");
+		driver.findElement(By.xpath("//*[@id='login-form']/fieldset/section[3]/label[2]/input")).sendKeys("Compro11");
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.id("submitButton")).click();
 	}
 
-	@AfterSuite
-	public void quitDriver() throws Exception {
-		driver.quit();
-	}
 }
